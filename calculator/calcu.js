1634d3e99299500dc2a4b4a1cf219fc1f9e9bba0
Vishnuraj1542 <vishnurajcheerankuzhi@gmail.com>
2024-01-06 10:32:03 +0530

    firstcommit

---
+++ calculator/calcu.js
@@ -0,0 +1,11 @@
+function num(val){
+    document.getElementById("screen").value=document.getElementById("screen").value+val
+}
+function rubber(){
+        document.getElementById("screen").value=""
+}
+function equalclick(){
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
